575addedca43a1054e270ad6afaa6efa108d8688
sneakyturtle2007 <103067582+sneakyturtle2007@users.noreply.github.com>
2023-07-07 09:23:14 -0500

    Delete DementiaProjectServer directory

--- DementiaProjectServer/ServerManager.java
+++
@@ -1,9 +0,0 @@
-import java.net.ServerSocket;
-
-public class ServerManager extends Thread{
-   /*  ServerSocket serverSocket = new ServerSocket(1234);
-    while(true)
-        new Main();
-        */
-    
-}
